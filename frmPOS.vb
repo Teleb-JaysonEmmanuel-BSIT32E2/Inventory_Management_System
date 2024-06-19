7e7d5a91ea48c382848dedac9a8b7f2fa96c0d74
MARAVILLA-JYROJHUSHAM-BSIT32E2 <136304245+MARAVILLA-JYROJHUSHAM-BSIT32E2@users.noreply.github.com>
2024-06-19 15:19:28 +0800

    Update frmPOS.vb

--- frmPOS.vb
+++ frmPOS.vb
@@ -1,4 +1,7 @@
-﻿Public Class frmPOS
+﻿Imports System.Data.OleDb
+Imports System.Data.SqlClient
+
+Public Class frmPOS
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         lblDate.Text = Now.ToLongDateString
         lblTime.Text = Now.ToLongTimeString
@@ -14,5 +17,25 @@
 
     Private Sub btnPayment_Click(sender As Object, e As EventArgs) Handles btnPayment.Click
         frmManagePayment.Show()
+    End Sub
+
+    Private Sub frmPOS_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call connection()
+        Call getTransactionNumber()
+    End Sub
+
+    Private Sub getTransactionNumber()
+        Try
+            sql = "Select TransactionNumber from tblSales Order By TransactionNumber Desc"
+            cmd = New OleDbCommand(sql, cn)
+            dr = cmd.ExecuteReader
+            If dr.Read = True Then
+                lblTransactNo.Text = Val(dr(0)) + 1
+            Else
+                lblTransactNo.Text = 1
+            End If
+        Catch ex As Exception
+            MsgBox("An error occurred frmPOS(TransactionNumber): " & ex.Message)
+        End Try
     End Sub
 End Class
